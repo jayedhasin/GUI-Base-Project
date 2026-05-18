f4bd9986b1e80e5f2af12c0f886ed23fa9de8c4c
jayedhasin <jayedhasin25@gmail.com>
2026-05-19 00:39:49 +0600

    Add files via upload

---
+++ Start.java
@@ -0,0 +1,12 @@
+// Main entry point
+import java.lang.*;
+import Frame.*;
+
+public class Start
+{
+    public static void main(String[] args)
+    {
+        HospitalFrame obj1 = new HospitalFrame();
+        obj1.setVisible(true);
+    }
+}
